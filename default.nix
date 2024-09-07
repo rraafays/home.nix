1bf186655e492226325b7fb0fa5df7fd88d02a14
raf <rraf@tuta.io>
2024-09-07 17:55:07 +0100

    chore: formatting

--- default.nix
+++ default.nix
@@ -1,44 +1,48 @@
 { pkgs, lib, ... }:
 
 let
-    USER = "raf";
+  USER = "raf";
 in
 {
   nixpkgs.config = {
     allowUnfree = true;
     packageOverrides = pkgs: {
-      nur = import (fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") { inherit pkgs; };
-      unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") { };
+      nur = import (fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+        inherit pkgs;
+      };
+      unstable =
+        import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz")
+          { };
     };
   };
 
   home-manager.users.${USER} = {
     home = {
-        enableNixpkgsReleaseCheck = false;
-        packages = with pkgs; [
-          adbfs-rootless
-          android-tools
-          jetbrains.idea-ultimate
-          mprocs
-          mpv
-          typer
-          xdg-utils
-          soulseekqt
-          xdg-user-dirs
-        ];
+      enableNixpkgsReleaseCheck = false;
+      packages = with pkgs; [
+        adbfs-rootless
+        android-tools
+        jetbrains.idea-ultimate
+        mprocs
+        mpv
+        typer
+        xdg-utils
+        soulseekqt
+        xdg-user-dirs
+      ];
       sessionVariables = {
-          XDG_DESKTOP_DIR = "$HOME/Desktop";
-          XDG_DOWNLOAD_DIR = "$HOME/Downloads";
-          XDG_TEMPLATES_DIR = "$HOME/Templates";
-          XDG_PUBLICSHARE_DIR = "$HOME/Public";
-          XDG_DOCUMENTS_DIR = "$HOME/Documents";
-          XDG_MUSIC_DIR = "$HOME/Music";
-          XDG_PICUTRES_DIR = "$HOME/Pictures";
-          XDG_VIDEOS_DIR = "$HOME/Videos";
+        XDG_DESKTOP_DIR = "$HOME/Desktop";
+        XDG_DOWNLOAD_DIR = "$HOME/Downloads";
+        XDG_TEMPLATES_DIR = "$HOME/Templates";
+        XDG_PUBLICSHARE_DIR = "$HOME/Public";
+        XDG_DOCUMENTS_DIR = "$HOME/Documents";
+        XDG_MUSIC_DIR = "$HOME/Music";
+        XDG_PICUTRES_DIR = "$HOME/Pictures";
+        XDG_VIDEOS_DIR = "$HOME/Videos";
       };
       activation.xdg-user-dirs = lib.mkAfter ''
-    ${pkgs.xdg-user-dirs}/bin/xdg-user-dirs-update
+        ${pkgs.xdg-user-dirs}/bin/xdg-user-dirs-update
       '';
-        };
+    };
   };
 }
